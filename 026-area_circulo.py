352f97d33f4750543215ff4a39d1328d7a9dd705
Antônio Alexandre Cavalcante Leite <aacleite@gmail.com>
2026-01-04 14:48:22 -0300

    Adicionando o algoritmo 026

---
+++ 026-area_circulo.py
@@ -0,0 +1,11 @@
+pi = 3.14159
+
+
+def area(raio):
+    return pi * raio**2
+
+
+if __name__ == "__main__":
+    raio = float(input("Digite o raio do círculo --> "))
+    resultado = area(raio)
+    print(f"Área do cículo de raio {raio:.2f} é {resultado:.2f}ua")
